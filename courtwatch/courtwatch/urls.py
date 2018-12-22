09c90fe443c0a2c750f6fcc606ad2aba078c05a6
Brian Tackett <briantac@buffalo.edu>
2018-12-22 14:05:50 -0500

    Initial outline

---
+++ courtwatch/courtwatch/urls.py
@@ -0,0 +1,39 @@
+"""courtwatch URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from courtwatchsite	import views
+from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # /stories/
+    url(r'^stories/$', views.stories, name='stories'),
+    # /statistics/
+    url(r'^statistics/$', views.statistics, name='statistics'),
+    # /resources/
+    url(r'^resources/$', views.resources, name='resources'),
+    # /join/
+    url(r'^join/$', views.join, name='join'),
+    # /contact/
+    url(r'^contact/$', views.contact, name='contact'),
+    # /login/
+    url(r'^login/$', views.login, name='login'),
+    url(r'^$', views.index, name='index'),
+]
+
+urlpatterns += staticfiles_urlpatterns()
---
+++ courtwatch/courtwatchsite/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, render_to_response
+
+# Create your views here.
+
+
+
+context = { }
+
+def index(request):
+	return render(request, 'index.html', context)
+
+def stories(request):
+    return render(request, "stories.html", context)
+
+
+def statistics(request):
+    return render(request, 'statistics.html', context)
+
+
+def resources(request):
+    return render(request, 'resources.html', context)
+
+def join(request):
+	return render_to_response('join.html')
+def contact(request):
+	return render_to_response('contact.html')
+def login(request):
+	return render_to_response('login.html')
+
+
