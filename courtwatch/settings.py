7f9b29973a8b3e9b3117877a0870646535d0e1d5
Mike Brown <mpbrown@buffalo.edu>
2019-01-21 19:36:27 -0500

    Attempts at working on Heroku implementation

--- courtwatch/settings.py
+++ courtwatch/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 from secrets import *
 import os
-import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,6 +131,5 @@ LOGIN_REDIRECT_URL = '../member/'
 
 LOGIN_URL = 'login'
 
-django_heroku.settings(locals())
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
