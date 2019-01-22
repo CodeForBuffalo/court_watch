e1d681623f2b0b1d38fa14c8bc8186cbc4b6a033
Mike Brown <mpbrown@buffalo.edu>
2019-01-21 19:37:04 -0500

    Revert "Added files for Heroku deployment"
    This reverts commit 195ebf45e788cd653ebc1021bb64f91ed22a3b27.

--- courtwatch/courtwatch/settings.py
+++ courtwatch/courtwatch/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-from secrets import *
+
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,5 +129,3 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '../member/'
 
 LOGIN_URL = 'login'
-
-django_heroku.settings(locals())
